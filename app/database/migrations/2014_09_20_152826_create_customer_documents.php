dbffc6193d93015c3b09b6bcbf15b8e2ebad2e6a
Chris Armitage <dev@chrisarmitage.com>
2014-09-20 17:10:06 +0100

    Customer documents migration

---
+++ app/database/migrations/2014_09_20_152826_create_customer_documents.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerDocuments extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('customer_documents', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('reference_fk');
+            $table->string('type');
+            $table->string('path');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('customer_documents');
+	}
+
+}
